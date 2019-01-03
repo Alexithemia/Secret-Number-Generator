ec65985e7862b707642fb7b682c5d86093a3aa53
Tyler <alexithemia@gmail.com>
2019-01-02 17:13:01 -1000

    complete

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,8 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  let num = (Math.random() * 1000000);
+  const secretNumber = function () {
+    return num;
+  }
+  return secretNumber;
 };
